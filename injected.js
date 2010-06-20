44d16abaf22c730b9ac9c8bf7eb283619394b184
pjv <pjv@cyberplexus.com>
2010-06-20 14:38:55 -0700

    change button and menu to a toggle

--- injected.js
+++ injected.js
@@ -1,6 +1,5 @@
 // here we have a bit of a hack to put an indication on the tab of it being unread.
-// it would be nice if apple, in their corporate glory,
-// would let us play with the chrome of the tabs so i could put a little img on it.
+// it would be nice if apple, would let us play with the chrome of the tabs so i could put a little img on it.
 // it would be a tasteful little red dot on the right-hand side...
 
 safari.self.addEventListener("message", handleMessage, false);
