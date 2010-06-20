c4403f178d63a035904e6bb7b7efb68843b22728
pjv <pjv@cyberplexus.com>
2010-06-20 14:21:35 -0700

    add toolbar button. fix bar update on tab navigation. mark tab as unread / read

--- injected.js
+++ injected.js
@@ -1,28 +1,39 @@
-safari.self.addEventListener("message", handleMessage, false);
-
-// here we have a monstrous hack to put an indication on the tab of it being unread.
+// here we have a bit of a hack to put an indication on the tab of it being unread.
 // it would be nice if apple, in their corporate glory,
 // would let us play with the chrome of the tabs so i could put a little img on it.
 // it would be a tasteful little red dot on the right-hand side...
 
-// well, i'll leave this in here in case anyone wants to pursue it, but it turns out that
-// apple is also not exposing events like clicking on a tab to the extension api
-// so there is also no way to mark the tab as read after the user clicks on it. baaahhh.
+safari.self.addEventListener("message", handleMessage, false);
+window.addEventListener("load", handleLoad, false);
 
+var marker = "\u2666 ";
+var markerStatus = null;
+
+function handleLoad(){
+    getMarkerStatus();
+}
 
 function setTitleUnread() {
-    // if not already, prepend title with [*]
+    // if not already, prepend title with marker
     myTitle = document.title;
-    if (! (myTitle.substr(0,4) == "[*] "))
-        document.title = "[*] " + myTitle;
-    }
+    if (! (myTitle.substr(0,2) == marker))
+        document.title = marker + myTitle;
+}
 
 function setTitleRead() {
-    // if title begins [*], get rid of it
+    // if title begins marker, get rid of it
     myTitle = document.title;
-    if (myTitle.substr(0,4) == "[*] ")
-        document.title = myTitle.substr(4,myTitle.length);
+    if (myTitle.substr(0,2) == marker) {
+        document.title = myTitle.substr(2,myTitle.length);
     }
+    else {
+        document.title = myTitle;
+    }
+}
+    
+function getMarkerStatus () {
+    safari.self.tab.dispatchMessage("sendMarkerStatus");
+}
 
 function handleMessage(msgEvent) {
 
@@ -34,6 +45,11 @@ function handleMessage(msgEvent) {
 
         case "setTitleRead":
             setTitleRead();
+            break;
+            
+        case "markerStatus":
+            if (msgEvent.message == "unread")
+                setTitleUnread();
             break;
     } // switch
 } // handleMessage
