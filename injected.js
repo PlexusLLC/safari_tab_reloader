5c9a00cee6cb838526b97ac6e2ccadad2b471a18
pjv <pjv@cyberplexus.com>
2010-08-07 12:23:54 -0700

    fix title weirdness (bug #9). implement vertical scroll remembering (bug #10)

--- injected.js
+++ injected.js
@@ -1,18 +1,47 @@
 // here we have a bit of a hack to put an indication on the tab of it being unread.
 // it would be nice if apple, would let us play with the chrome of the tabs so i could put a little img on it.
-// it would be a tasteful little green diamond on the right-hand side...
-
-if (window.top === window)
-    safari.self.addEventListener("message", handleMessage, false);
+if (window === window.top) {
+	safari.self.addEventListener("message", handleMessage, false);
+	window.addEventListener("load", _reloadMe, false);
+}
 
 function handleMessage(msgEvent) {
-    // don't run in anything other than the main page
-    if (window !== window.top) return;
+	switch (msgEvent.name) {
 
-    switch (msgEvent.name) {
+	case "setTitle":
+		document.title = msgEvent.message;
+		break;
 
-        case "setTitle":
-            document.title = msgEvent.message;
-            break;
-    } // switch
+	case "reload":
+		reloadMe();
+		break;
+
+	} // switch
 } // handleMessage
+
+function reloadMe() {
+	var scrollPos = window.pageYOffset;
+	var myURL = location.href;
+
+	// get rid of existing scroll in url
+	myURL = myURL.replace(/.scroll=\d+/g, '');
+
+	if (scrollPos != 0) {
+		if (myURL.indexOf('?') == -1) myURL += "?scroll=" + scrollPos.toString();
+		else myURL += "&scroll=" + scrollPos.toString();
+	}
+	location.href = myURL;
+}
+
+function _reloadMe() {
+	// parse URL and reload to scroll parameter
+	var search = window.location.search;
+	// if query string exists  
+	if (search) {
+		// find scroll parameter in query string  
+		var matches = /scroll=(\d+)/.exec(search);
+		// jump to the scroll position if scroll parameter exists  
+		if (matches) window.scrollTo(0, matches[1]);
+	}
+	safari.self.tab.dispatchMessage("getTitle");
+}
